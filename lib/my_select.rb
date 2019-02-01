8deb2247ca60080e865595610cb0189221fcb2d7
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-01 05:22:38 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
- # your code here!
+  new_collection = []
+  if collection.size == 0
+    return new_collection
+  else
+    i = 0
+    while i < collection.size
+      element = collection[i]
+      if element % 2 == 0
+        new_collection << yield(element)
+      end
+      i += 1
+    end
+  end
+  new_collection
 end
+
+
+
